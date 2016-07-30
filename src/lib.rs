4ae0cb78d81c8d6a01d2e02a75899a733b954619
Bruce Mitchener <bruce.mitchener@gmail.com>
2016-07-30 22:55:15 +0700

    Sketch.

---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! # URI Prefix Mapping
+//!
+
+#![warn(missing_docs)]
+#![deny(trivial_numeric_casts,
+        unsafe_code, unstable_features,
+        unused_import_braces, unused_qualifications)]
+
+use std::collections::HashMap;
+
+#[allow(missing_docs)]
+pub struct PrefixMapping<'pm> {
+    mapping: HashMap<&'pm str, &'pm str>,
+}
+
+#[allow(missing_docs)]
+impl<'pm> PrefixMapping<'pm> {
+    pub fn add_ns_prefix(&mut self, prefix: &'pm str, uri: &'pm str) {
+        self.mapping.insert(prefix, uri);
+    }
+
+    pub fn remove_ns_prefix(&mut self, prefix: &str) {
+        self.mapping.remove(prefix);
+    }
+
+    pub fn get_ns_prefix_uri(&self, prefix: &str) -> Option<&&str> {
+        self.mapping.get(prefix)
+    }
+
+    pub fn get_ns_uri_prefix(&self, _uri: &str) -> Option<&&str> {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {}
+}
