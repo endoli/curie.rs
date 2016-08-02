e841bb3cc8bcd4a6be504e9d4d0760b6b78b3454
Bruce Mitchener <bruce.mitchener@gmail.com>
2016-08-02 17:24:16 +0700

    Rebuild documentation at 0351ad6

---
+++ curie/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["PrefixMappingError","Errors that might occur during CURIE expansion."]],"struct":[["PrefixMapping","Maps prefixes to base URIs and allows for the expansion of CURIEs (Compact URIs)."]]});
---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["curie"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='curie/enum.PrefixMappingError.html' title='curie::PrefixMappingError'>PrefixMappingError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
