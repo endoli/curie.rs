7bb921fcf0f56690b98d55930a70d9ed248bbcc1
Bruce Mitchener <bruce.mitchener@gmail.com>
2016-08-03 09:21:47 +0700

    Rebuild documentation at 3009109

--- curie/sidebar-items.js
+++ curie/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["PrefixMappingError","Errors that might occur during CURIE expansion."]],"struct":[["PrefixMapping","Maps prefixes to base URIs and allows for the expansion of CURIEs (Compact URIs)."]]});
+initSidebarItems({"enum":[["ExpansionError","Errors that might occur during CURIE expansion."],["InvalidPrefixError","Errors that might occur when adding a prefix to a `PrefixMapping`."]],"struct":[["PrefixMapping","Maps prefixes to base URIs and allows for the expansion of CURIEs (Compact URIs)."]]});
--- implementors/core/fmt/trait.Debug.js
+++ implementors/core/fmt/trait.Debug.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["curie"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='curie/enum.PrefixMappingError.html' title='curie::PrefixMappingError'>PrefixMappingError</a>",];
+implementors["curie"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='curie/enum.InvalidPrefixError.html' title='curie::InvalidPrefixError'>InvalidPrefixError</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='curie/enum.ExpansionError.html' title='curie::ExpansionError'>ExpansionError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
